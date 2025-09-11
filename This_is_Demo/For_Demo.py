6dce0d302da5afad87c7f672890278d1b0986723
Bhagwant_scs <scs.bhagwant.dcs@gmail.com>
2025-09-11 20:56:23 +0530

    Issue in Code for Ember dataset

--- This_is_Demo/For_Demo.py
+++ This_is_Demo/For_Demo.py
@@ -1,1 +1,3 @@
-Hello ("This is the Demo Code")
+def add_numbers(first_number: float, second_number: float) -> float:
+    """Return the sum of two numbers without printing anything."""
+    return first_number + second_number
